67b129805d243cc820d022173fcbc7489bfb7419
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-04-09 13:56:35 +0200

    Added Typed JS

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ $(document).ready(function() {
 		play: 5000,
 		pagination: false,
 	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Web Developer. ^1000", "Software Engineer. ^1000", "Student. ^1000"],
+		typeSpeed: 48,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
 });
+

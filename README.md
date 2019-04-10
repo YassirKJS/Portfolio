0e775d31c41a31bd87c3615721a9df158214ea9c
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-04-10 15:28:34 +0200

    ???

--- README.md
+++
@@ -1,7 +0,0 @@
-In this project I'll be making my own portfolio website.
-
-What I worked with so far:
--Git BASH ( https://gitforwindows.org/ )
--Superslides ( https://github.com/nicinabox/superslides )
--Typed.js ( https://github.com/mattboldt/typed.js/ )
--Bootstrap ( https://getbootstrap.com/ )
--- js/script.js
+++ js/script.js
@@ -12,5 +12,60 @@ $(document).ready(function() {
 		startDelay: 1000,
 		showCursor: false
 	});
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
+
+	
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    var statsTopOffset = $(".statsSection").offset().top;
+    var countUpFinished = false;
+
+    $(window).scroll(function() {
+    	if (window.pageYOffset > skillsTopOffset - $(window).height() + 220) {
+    		$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor:false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
+    	}
+
+    	var str1 = '#count-test-';
+		var str2 = 'count-test-';
+    	if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 220) {
+			for (var i = 1; i < 5; i++) {
+				$(str1.concat(i)).jQuerySimpleCounter({
+				start:  0,
+		        end:    document.getElementById(str2.concat(i)).innerHTML,
+				duration: 1500
+				});
+			}
+			countUpFinished = true;
+    	}
+    });
+	
+
 });
 

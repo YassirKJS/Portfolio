c3446ddda43420dd7c2411e9f36bf5f0e12fc9f4
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-04-11 16:40:52 +0200

    ???

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+$(window).on("load", function() {
+	$(".loader .inner").fadeOut(500, function() {
+		$(".loader").fadeOut(750);
+	});
+
+	$(".items").isotope({
+		filter: '*', //filter applied right from the start is all*
+		animationOptions: {
+			duration: 1500,
+			easing: 'linear',
+			queue: false
+		}
+	});
+});
+
+
 $(document).ready(function() {
 	$('#slides').superslides({
 		animation: 'fade',
@@ -66,6 +82,53 @@ $(document).ready(function() {
     	}
     });
 	
+	$("[data-fancybox]").fancybox();
+
+	
+
+	/*# is used before the id*/
+	$("#filters a").click(function() {
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data-filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+		return false; /*false => dont do anything else*/
+	});
+
+
+	$("#navigation li a").click(function(e) {
+		e.preventDefault(); //prevent the default click event
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+	});
+
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+		var body = $("body");
+		if ($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px");
+			body.addClass("fixedNav");	
+		}
+		else {
+			body.css("padding-top", 0);
+			body.removeClass("fixedNav");
+		}
+	}
 
 });
 
